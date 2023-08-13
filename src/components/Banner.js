a98a45fe4883a40316b10bf285c986179227a142
wcgarant <107452402+wcgarant@users.noreply.github.com>
2023-08-13 12:50:39 -0400

    completed assignment, styling, tests

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import './styling.css';
+
+export default function Banner() {
+  return (
+    <div>
+      <header>Orbit Report</header>
+      <p>Click on the buttons to see the satellites in that orbit type</p>
+    </div>
+  );
+}
